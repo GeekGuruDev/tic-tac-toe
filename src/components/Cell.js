0f4a1640435cde611df9d3d6421d7695901cbc5d
Gurpreet Singh <progurpreetsingh@gmail.com>
2022-03-29 11:34:59 +0530

    Change favicon and X/O size

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,7 @@
 const Cell = ({ children, onClick, className }) => {
   return (
     <button
-      className={`bg-indigo-400 text-slate-200 text-4xl w-24 h-24 rounded ${className} hover:bg-indigo-500`}
+      className={`bg-indigo-400 text-slate-200 text-6xl w-24 h-24 rounded ${className} hover:bg-indigo-500`}
       onClick={onClick}
     >
       {children}
